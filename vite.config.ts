cf150bf8cc12ad383cfebde8274c02f5b0cacb51
matheuslanduci <matheuslanduci@gmail.com>
2025-11-16 12:40:25 -0300

    remove cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
@@ -9,15 +8,5 @@ export default defineConfig({
 	server: {
 		port: 3000
 	},
-	plugins: [
-		cloudflare({
-			viteEnvironment: {
-				name: 'ssr'
-			}
-		}),
-		tailwindcss(),
-		tsConfigPaths(),
-		tanstackStart(),
-		viteReact()
-	]
+	plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), viteReact()]
 })
